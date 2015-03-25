90cff365e925379a22c16b7580d5e5c8c48c91ba
YJ Yang <chcokr@gmail.com>
2015-03-25 02:54:37 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/bash
+
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
+brew install \
+  boost	\
+  caskroom/cask/brew-cask \
+  cmake	\	
+  git \
+  leiningen	\
+  mongodb \
+  mysql \
+  nmap \
+  node \
+  openssl \
+  privoxy \
+  pssh \
+  redis \
+  tor \
+  vtk \
+;
+brew cask install \
+  clion-eap \
+  evernote \
+  firefox \
+  google-chrome \
+  java \
+  kindle \
+  mactracker \
+  microsoft-office \
+  sequel-pro \
+  skype \
+  silverlight \
+  spectacle \
+  spotify \
+  sublime-text \
+  virtualbox \
+  vlc \
+  vmware-fusion \
+  webstorm \
+;
