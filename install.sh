e2ade2929b1cd513a4baec6d352097b93b82716d
YJ Yang <chcokr@gmail.com>
2015-04-06 18:43:26 -0400

    Add line moving Dock to the right side

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ defaults write com.apple.dock tilesize -int 36; # smaller icon sizes in Dock
 defaults write com.apple.dock autohide -bool true; # turn Dock auto-hidng on
 defaults write com.apple.dock autohide-delay -float 0; # remove Dock show delay
 defaults write com.apple.dock autohide-time-modifier -float 0; # remove Dock show delay
+defaults write com.apple.dock orientation right; # place Dock on the right side of screen
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true; # show all file extensions
 killall Dock;
 killall Finder;
