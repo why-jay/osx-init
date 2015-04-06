bd901d60873c14631ebfc92ce5e3ab7acff19059
YJ Yang <chcokr@gmail.com>
2015-04-06 18:29:37 -0400

    Add some Dock and Finder defaults

--- install.sh
+++ install.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/bash
 
 defaults write com.apple.finder AppleShowAllFiles YES; # show hidden files
-
+defaults write com.apple.dock persistent-apps -array; # remove icons in Dock
+defaults write com.apple.dock tilesize -int 36; # smaller icon sizes in Dock
+defaults write com.apple.dock autohide -bool true; # turn Dock auto-hidng on
+defaults write com.apple.dock autohide-delay -float 0; # remove Dock show delay
+defaults write com.apple.dock autohide-time-modifier -float 0; # remove Dock show delay
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true; # show all file extensions
+killall Dock;
+killall Finder;
+  
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
 brew install \
   boost	\
