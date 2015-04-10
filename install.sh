a5bb249052c5fa848208ef4847a8c6c462cf7440
YJ Yang <chcokr@gmail.com>
2015-04-10 04:48:13 -0400

    Add Xcode Command Line Tools installation script

--- install.sh
+++ install.sh
@@ -11,6 +11,16 @@ defaults write NSGlobalDomain AppleShowAllExtensions -bool true; # show all file
 killall Dock;
 killall Finder;
 
+# install Xcode Command Line Tools
+# https://github.com/timsutton/osx-vm-templates/blob/ce8df8a7468faa7c5312444ece1b977c1b2f77a4/scripts/xcode-cli-tools.sh
+touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
+PROD=$(softwareupdate -l |
+  grep "\*.*Command Line" |
+  head -n 1 | awk -F"*" '{print $2}' |
+  sed -e 's/^ *//' |
+  tr -d '\n')
+softwareupdate -i "$PROD" -v;
+
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
 brew install \
   boost	\
