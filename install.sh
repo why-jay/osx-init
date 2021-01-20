023cdeeee4fc743499d4ae9ea721e9faa3b84d85
Andy Herbota <andy@herbota.com>
2021-01-20 14:10:32 +0800

    Ensure Xcode CLI Tools has most recent version selected

--- install.sh
+++ install.sh
@@ -23,14 +23,14 @@ echo "highlight OverLength ctermbg=red ctermfg=white guibg=#592929" >> ~/.vimrc;
 echo "match OverLength /\\%81v.\\+/" >> ~/.vimrc;
 
 # install Xcode Command Line Tools
-# https://github.com/timsutton/osx-vm-templates/blob/ce8df8a7468faa7c5312444ece1b977c1b2f77a4/scripts/xcode-cli-tools.sh
+# https://github.com/timsutton/osx-vm-templates/blob/dc2d3b29a12cfecb40d5ca6b839bd4c9c48f70bc/scripts/xcode-cli-tools.sh
 touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
 PROD=$(softwareupdate -l |
   grep "\*.*Command Line" |
-  head -n 1 | awk -F"*" '{print $2}' |
+  tail -n 1 | awk -F"*" '{print $2}' |
   sed -e 's/^ *//' |
   tr -d '\n')
-softwareupdate -i "$PROD" -v;
+softwareupdate -i "$PROD" --verbose;
 
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
 brew install \
